08369b1e9739a18bb89a74d276f7d34b9200dddd
Lucas Eleutério Bueno <leleuteiobueno@gmail.com>
2021-11-23 16:20:26 -0300

    adding mui dependencies and enabling fast reload

--- src/index.js
+++ src/index.js
@@ -2,24 +2,17 @@
 import React from "react";
 //Base import for the main .js in the project
 import reactDom from "react-dom";
+import { Typography } from "@mui/material";
 
 //This function is a React Component, all components functions need to start with captilized letter
 function Greeting() {
   //Every component should return some JSX, basically the React Html
   return (
     <div>
-      <Person />
-      <Message />
+      <h1>Teste</h1>
+      <Typography>Teste de tipografia</Typography>
     </div>
   );
-}
-
-function Person() {
-  return <h1>Hello i'm Lucas</h1>;
-}
-
-function Message() {
-  return <h2>And i'm a Junior Dev</h2>;
 }
 
 //Here we use the ReactDom to start the aplication, used primary in the root Component, this one
