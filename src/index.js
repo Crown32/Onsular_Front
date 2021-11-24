ee69fbf41a662fa98e30e328389f92ea42148893
Lucas Eleutério Bueno <leleuteiobueno@gmail.com>
2021-11-24 09:11:24 -0300

    Adding login page

--- src/index.js
+++ src/index.js
@@ -3,17 +3,14 @@ import React from "react";
 //Base import for the main .js in the project
 import reactDom from "react-dom";
 import { Typography } from "@mui/material";
-
+import SignIn from "./login";
 //This function is a React Component, all components functions need to start with captilized letter
 function Greeting() {
   //Every component should return some JSX, basically the React Html
   return (
-    <div>
-      <h1>Teste</h1>
-      <Typography>Teste de tipografia</Typography>
-    </div>
+     <SignIn></SignIn> 
   );
 }
 
-//Here we use the ReactDom to start the aplication, used primary in the root Component, this one
+//Here we use the ReactDom to start the aplication , used primary in the root Component, this one
 reactDom.render(<Greeting />, document.getElementById("root"));
