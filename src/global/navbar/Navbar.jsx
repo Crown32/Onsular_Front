4e4ce45a07ef512b8bad27d26d038f7d0d8cec08
Lucas Eleutério Bueno <leleuteiobueno@gmail.com>
2021-11-27 11:46:48 -0300

    Adding 5ppt spacing on bottom

--- src/global/navbar/Navbar.jsx
+++ src/global/navbar/Navbar.jsx
@@ -156,7 +156,7 @@ export default function PrimarySearchAppBar() {
   );
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box sx={{ flexGrow: 1, marginBottom: 5 }}>
       <AppBar position='static'>
         <Toolbar>
           <MyDrawer></MyDrawer>
